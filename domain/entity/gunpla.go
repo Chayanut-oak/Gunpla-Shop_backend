89e1db60ddd9273f7a75ffa952918ee20e093488
Chayanut-oak <chayanut.oakley@gmail.com>
2024-03-08 18:36:45 +0700

    update entity

--- domain/entity/gunpla.go
+++ domain/entity/gunpla.go
@@ -6,6 +6,7 @@ type Gunpla struct {
 	Name        string   `json:"name"`
 	Type        string   `json:"type"`
 	Series      string   `json:"series"`
+	Brand       string   `json:"brand"`
 	Scale       string   `json:"scale"`
 	Grade       string   `json:"grade"`
 	Price       int64    `json:"price"`
